2db33f56d2dcf631230228251884ef0dca1ad678
Jonathan Fernandes <jhfernan@mail.usf.edu>
2019-01-07 00:00:32 -0500

    Updated for heroku

--- plugins/app.js
+++ plugins/app.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
+import Vue from "vue"
 
-import Markdown from "vue-markdown";
-import Profile from "~/components/app/profile.vue";
-import Title from "~/components/app/titles.vue";
+import Markdown from "vue-markdown"
+import Profile from "~/components/app/profile.vue"
+import Title from "~/components/app/titles.vue"
 
-Vue.component("markdown", Markdown);
-Vue.component("c-profile", Profile);
-Vue.component("c-title", Title);
+Vue.component("markdown", Markdown)
+Vue.component("c-profile", Profile)
+Vue.component("c-title", Title)
